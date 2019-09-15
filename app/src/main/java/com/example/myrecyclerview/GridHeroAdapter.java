559a61242dfa2c4f95178684dc06b64313e66f09
indahratnasari <indahrs05@gmail.com>
2019-09-15 16:55:56 +0700

    Menambah cardView

--- app/src/main/java/com/example/myrecyclerview/GridHeroAdapter.java
+++ app/src/main/java/com/example/myrecyclerview/GridHeroAdapter.java
@@ -16,24 +16,35 @@ import java.util.ArrayList;
 public class GridHeroAdapter extends RecyclerView.Adapter<GridHeroAdapter.GridViewHolder> {
 
     private ArrayList<Hero> listHero;
+    private OnItemClickCallback onItemClickCallback;
 
-    public GridHeroAdapter(ArrayList<Hero> list) {
+    void setOnItemClickCallback(OnItemClickCallback onItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback;
+    }
+    GridHeroAdapter(ArrayList<Hero> list) {
         this.listHero = list;
     }
 
     @NonNull
     @Override
-    public GridViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+    public GridHeroAdapter.GridViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_grid_hero, viewGroup, false);
         return new GridViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final GridViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final GridHeroAdapter.GridViewHolder holder, int position) {
         Glide.with(holder.itemView.getContext())
                 .load(listHero.get(position).getPhoto())
                 .apply(new RequestOptions().override(350, 550))
                 .into(holder.imgPhoto);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClickCallback.onItemClicked(listHero.get(holder.getAdapterPosition()));
+            }
+        });
     }
 
     @Override
@@ -48,5 +59,9 @@ public class GridHeroAdapter extends RecyclerView.Adapter<GridHeroAdapter.GridVi
             super(itemView);
             imgPhoto = itemView.findViewById(R.id.img_item_photo);
         }
+    }
+
+    public interface OnItemClickCallback {
+        void onItemClicked(Hero data);
     }
 }
--- app/src/main/java/com/example/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -15,11 +15,13 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView rvHeroes;
     private ArrayList<Hero> list = new ArrayList<>();
+    private String title = "Mode List";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setActionBarTitle(title);
 
         rvHeroes = findViewById(R.id.rv_heroes);
         rvHeroes.setHasFixedSize(true);
@@ -44,21 +46,41 @@ public class MainActivity extends AppCompatActivity {
         setMode(item.getItemId());
         return super.onOptionsItemSelected(item);
     }
+
     public void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.action_list:
+                title = "Mode List";
                 showRecyclerList();
                 break;
+
             case R.id.action_grid:
+                title = "Mode Grid";
                 showRecyclerGrid();
                 break;
+
             case R.id.action_cardview:
+                title = "Mode CardView";
+                showRecyclerCardView();
                 break;
         }
     }
+
     private void showRecyclerGrid(){
         rvHeroes.setLayoutManager(new GridLayoutManager(this, 2));
         GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
         rvHeroes.setAdapter(gridHeroAdapter);
+    }
+
+    private void showRecyclerCardView(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        CardViewHeroAdapter cardViewHeroAdapter = new CardViewHeroAdapter(list);
+        rvHeroes.setAdapter(cardViewHeroAdapter);
+    }
+
+    private void setActionBarTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
     }
 }
